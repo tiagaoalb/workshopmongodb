66643c980a8bad6c1e75ad26923be05490c8185c
Tiago <tiagaoalb@gmail.com>
2023-09-30 19:15:45 -0300

    feat: add Inserção de usuário com POST

--- src/main/java/com/tiagobovolin/workshopmongodb/services/UserService.java
+++ src/main/java/com/tiagobovolin/workshopmongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.tiagobovolin.workshopmongodb.services;
 
 import com.tiagobovolin.workshopmongodb.domain.User;
+import com.tiagobovolin.workshopmongodb.dto.UserDTO;
 import com.tiagobovolin.workshopmongodb.repository.UserRepository;
 import com.tiagobovolin.workshopmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
