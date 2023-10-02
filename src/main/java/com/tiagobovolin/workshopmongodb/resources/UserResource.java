336a7ecf8b0abb0f27ab377cb4b7b1825a63024f
Tiago <tiagaoalb@gmail.com>
2023-10-02 13:47:50 -0300

    feat: add Endpoint para retornar os posts de um usuário

--- src/main/java/com/tiagobovolin/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/tiagobovolin/workshopmongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.tiagobovolin.workshopmongodb.resources;
 
+import com.tiagobovolin.workshopmongodb.domain.Post;
 import com.tiagobovolin.workshopmongodb.domain.User;
 import com.tiagobovolin.workshopmongodb.dto.UserDTO;
 import com.tiagobovolin.workshopmongodb.services.UserService;
@@ -51,5 +52,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
